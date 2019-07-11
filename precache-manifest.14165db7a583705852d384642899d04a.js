892896066864e7d4c225c4588f8ec6069ed8f139
unknown <prekhanna2@gmail.com>
2019-07-11 01:13:44 -0400

    Updates

---
+++ precache-manifest.14165db7a583705852d384642899d04a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1258e66d0c060ea1478f89d483529bc",
+    "url": "/brew2/index.html"
+  },
+  {
+    "revision": "8a7bc8552d4abc7cd24b",
+    "url": "/brew2/static/css/main.a0acb7c3.chunk.css"
+  },
+  {
+    "revision": "cf5e06832d4f69f18781",
+    "url": "/brew2/static/js/2.127d5198.chunk.js"
+  },
+  {
+    "revision": "8a7bc8552d4abc7cd24b",
+    "url": "/brew2/static/js/main.1f41ba4a.chunk.js"
+  },
+  {
+    "revision": "c23eb538db1cae0a617d",
+    "url": "/brew2/static/js/runtime~main.da7b123c.js"
+  }
+]);
